f14462bcb7c05e206cb5b9bc56abea3311d4a656
faftech3210 <ffaere@byupathway.edu>
2025-06-07 13:02:34 +0100

    W05 Project: Site Plan WDD231

---
+++ scripts/plan.js
@@ -0,0 +1,5 @@
+  // Dynamic Copyright Year
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+    // Document Last Modified Date
+    document.getElementById("lastModified").textContent = `Last Updated: ${document.lastModified}`;
